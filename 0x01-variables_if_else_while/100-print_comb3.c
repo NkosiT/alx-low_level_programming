c8f90fb69d6469333fc1c0c8d264deb2b99aaf0e
NkosiT <thokoterris@gmail.com>
2023-06-18 16:54:16 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible differnt combinations of teo digits
+ * main - Prints all possible different combinations of two digits
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -10,7 +10,7 @@ int main(void)
 
 	for (n = 48; n <= 56; n++)
 	{
-		for (m = 49; n <= 57; m++)
+		for (m = 49; m <= 57; m++)i
 		{
 			if (m > n)
 			{
