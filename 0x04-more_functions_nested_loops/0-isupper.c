d0c0f1aa91e41d4137f9707c2d637a4223836b48
NkosiT <thokoterris@gmail.com>
2023-06-22 13:13:49 -0700

    isdigit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,13 @@
 /**
  * _isupper - Check if a letter is upper
  * @x: The number to be checked
- * Return: 1 for upper letter or 0 for any else
+ * Return: 1 for uppercase character or 0 for anything else
  */
 
 int _isupper(int x)
 
 {
-	if (x >= 65 && x <= 98)
+	if (x >= 65 && x <= 90)
 	{
 	return (1);
 	}
